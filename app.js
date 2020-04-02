c2489dee748be108120cd4479d650fe9cb13f75e
Joseph Garcia <prosif@gmail.com>
2020-04-01 22:01:48 -0700

    basic perf output

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const RTCRelay = require('rtc-relay');
 
+const times = [];
+const output = document.getElementById('output');
+
 const relay = new RTCRelay(`ws://${window.location.hostname}`, () => {
     relay.send('i am a client');
 }, (msg) => {
-    console.log('got a message!');
-    console.log(msg);
+    times.push(Date.now());
+    if (times.length % 100 === 0) {
+        output.innerHTML = `Got 100 messages in ${times[times.length - 1] - times[times.length - 61]}ms`;
+    }
 }, 'arraybuffer');
